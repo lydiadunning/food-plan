b0cc494ac8dd9076ed445d2e4a38204356e78164
Lydia Dunning <lydia.dunning@gmail.com>
2023-08-09 14:16:12 -0700

    Add first draft addChild component, name only

--- src/components/addChild.jsx
+++ src/components/addChild.jsx
@@ -1,5 +1,36 @@
 // a component for adding a child, during app setup
 
-const addChild = ({  }) => {
+const addChild = ({ form, setForm }) => {
+  return(
+    <form className="new-child">
+      
 
+    </form>
+  )
 }
+
+import Entry from './Entry.jsx'
+
+const Inputs = ({ form, setForm, errors, setErrors }) => {
+
+  return(
+		<form className="input">
+      <label className="name" > Child
+      <input type='text' placeholder="name"
+        value={ form.name} onChange={ e =>  
+        { setForm({ ...form, name: e.target.value })
+
+        }} />
+      </label>
+      <label className="name" > Child
+      <input type='text' placeholder="name"
+        value={ form.name} onChange={ e =>  
+        { setForm({ ...form, name: e.target.value })
+        }} />
+      </label>
+
+	</form>
+	)
+}
+
+export default Inputs;
