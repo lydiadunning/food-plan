6b6d1570d0e7e5f6a5adbf97d3160dea848ff957
Lydia Dunning <lydia.dunning@gmail.com>
2023-10-29 12:09:02 -0700

    add logging to backend

--- backend/app.js
+++ backend/app.js
@@ -8,30 +8,36 @@ const tryHintRouter = require('./controllers/tryHints.js')
 const introRouter = require('./controllers/intros.js')
 const { log } = require('console')
 // const userRouter = require('./controllers/users')
+const logger = require('./utils/logger')
+const {  requestLogger,
+  unknownEndpoint,
+  errorHandler } = require('./utils/middleware.js')
 
 const app = express()
+app.use(express.static('build'))// what does the static method do?
+app.use(express.json())// what does the json method return?
+app.use(requestLogger)
 
 mongoose.set('strictQuery', false)
 // review why I'm doing this
-
-// connect to MongoDB
-mongoose.connect(config.MONGODB_URI)
-  // eslint-disable-next-line no-unused-vars
-  .then(result => {
-    console.log('connected to', config.MONGODB_URI)
-  })
-  .catch((error) => {
-    console.error('error connecting to MongoDB:', error.message)
-  })
-
+  
+  mongoose.connect(config.MONGODB_URI)
+    // eslint-disable-next-line no-unused-vars
+    .then(result => {
+      logger.info('connected to', config.MONGODB_URI)
+    })
+    .catch((error) => {
+      logger.error('error connecting to MongoDB:', error.message)
+    })
 
 app.use(cors())// what does the function cors() return?
 app.get('http://localhost:5173', cors(), function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for a Single Route'})
 })
 
-app.use(express.static('build'))// what does the static method do?
-app.use(express.json())// what does the json method return?
+
+
+
 
 // app.use the router - to direct requests
 app.use('/api/child', childRouter)
@@ -39,5 +45,8 @@ app.use('/api/child', childRouter)
 app.use('/api/try', tryRouter)
 app.use('/api/try-hint', tryHintRouter)
 app.use('/api/intro', introRouter)
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 module.exports = app
