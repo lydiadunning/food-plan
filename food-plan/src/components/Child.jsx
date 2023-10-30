a7ef315f40df000e98f2ac59665e93673e126ff4
Lydia Dunning <lydia.dunning@gmail.com>
2023-10-30 14:41:39 -0700

    fix child page with no intros

--- food-plan/src/components/Child.jsx
+++ food-plan/src/components/Child.jsx
@@ -7,13 +7,12 @@ const Child = ({ child, openAddIntro, closeChild }) => {
     return strings.toString()
   }
 
-  console.log('intros', child.intros[0])
 
   return (
     <>
       <p>{ child.name }</p>
       <p>{ listEach(child.tries, 'try') }</p>
-      { child.intros && <Intro introId={ child.intros[0] } /> }
+      { child.intros.length >= 1 && <Intro introId={ child.intros[0] } /> }
       <button onClick={ () => openAddIntro(child) }>add an introduction</button>
       {/* <button onClick={ deleteChild }>delete</button> */}
       <button onClick={ closeChild }>back to list</button>
