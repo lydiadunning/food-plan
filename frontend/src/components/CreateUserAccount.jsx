a8753e01bad0b74c4e08198e17bb3f40b9da2651
Lydia Dunning <lydia.dunning@gmail.com>
2023-12-15 23:09:57 -0800

    add components for user functionality

---
+++ frontend/src/components/CreateUserAccount.jsx
@@ -0,0 +1,34 @@
+import { useForm } from "react-hook-form"
+import { useCreateAccount } from "../serverStore/mutations"
+
+const CreateUserAccount = () => {
+  const {register, handleSubmit} = useForm()
+
+  
+  const createAccount = useCreateAccount()
+
+  const onSubmit = (data) => {
+    createAccount(data)
+    console.log('data', data)
+  }
+
+  return (
+    <>
+      <h1>Create a new user account </h1>
+      <form onSubmit={ handleSubmit(onSubmit) }>
+        <label htmlFor='username'>username</label>
+        <input id='username' type='text' required {...register('username')} />
+        <label htmlFor='name'>name</label>
+        <input id='name' type='text' required {...register('name')} />
+        <label htmlFor='email'>email</label>
+        <input id='email' type='text' {...register('email')}></input>
+        <label htmlFor='password'>password</label>
+        <input id='password' type='text' {...register('password')}></input>
+        <button type='submit'>submit</button>
+      </form>
+      <button onClick={ createAccount }>Back</button>
+    </>
+  )
+}
+
+export default CreateUserAccount
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,13 +1,33 @@
+import { useState } from 'react'
+import { useForm } from "react-hook-form"
+import CreateUserAccount from './CreateUserAccount'
+
+
 export function Login({ closeLogin }) {
+const { register, handleSubmit } = useForm()
+const { showCreateAccount, setShowCreateAccount } = useState(false)
+
+const onSubmit = (data) => {
+  //login
+  console.log('data', data)
+  closeLogin()
+}
+
   return (
     <>
-      <form>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label>username: </label>
-        <input></input>
+        <input id='username' type='text' required {...register('username')}></input>
         <label>password: </label>
-        <input></input>
+        <input id='password' type='text' required {...register('password')}></input>
       </form>
+      <button onClick={ () => setShowCreateAccount(!showCreateAccount)}>{showCreateAccount ? 'close create account' : 'create account'}</button>
+      { showCreateAccount && <CreateUserAccount/> }
       <button onClick={ closeLogin }>Back</button>
+
     </>
   )
 }
+
+
