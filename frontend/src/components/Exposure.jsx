1ed7a19da43d58f0a18b856a843bcdd9cf806095
Lydia Dunning <lydia.dunning@gmail.com>
2023-12-23 10:48:51 -0800

    convert exposure.date to dateString for display

--- frontend/src/components/Exposure.jsx
+++ frontend/src/components/Exposure.jsx
@@ -1,5 +1,7 @@
 const Exposure = ({ kidId, exposure, openEditExposures }) => {
 
+  const date = new Date(exposure.date) || null
+
   return (
     <section>
       <p>Food: { exposure.food }</p>
@@ -7,7 +9,7 @@ const Exposure = ({ kidId, exposure, openEditExposures }) => {
       {/* commenting out the outcome until it's converted into an array
        { exposure.outcome && <p>Tried: { exposure.outcome }</p> } */}
       { exposure.meal && <p>Meal: { exposure.meal }</p> }
-      { exposure.date && <p>Date: { exposure.date.toString() }</p>}
+      { exposure.date && <p>Date: { date.toDateString() }</p>}
       <button onClick={ () => openEditExposures(exposure) }>Edit Exposure</button>
     </section>
   )
