c38687f3fff4ee2921a1d6f4415d3dd5872067fc
Lydia Dunning <lydia.dunning@gmail.com>
2024-01-17 10:41:17 -0800

    move icons to svg file

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -1,14 +1,18 @@
 import * as Popover from '@radix-ui/react-popover';
 import { browserLogout } from './userAuth/userHooks';
 import { useState } from 'react';
-import { Button, Box, Flex } from '@radix-ui/themes'
-
+import { Button, Card, Flex } from '@radix-ui/themes'
+import { useQueryClient } from 'react-query';
+import { x } from '../assets/svgImages';
 
 const TopBar = ({ handleResetTo }) => {
   const [open, setOpen] = useState(false)
 
-  const logout = () => {
+  const queryClient = useQueryClient()
+
+  const logout = async () => {
     browserLogout()
+    await queryClient.invalidateQueries('kids')
     handleResetTo('login')
     setOpen(false)
   }
@@ -16,16 +20,19 @@ const TopBar = ({ handleResetTo }) => {
   return (
     <Flex justify='end' mb='3'>
       <Popover.Root open={open} onOpenChange={setOpen}>
+
+        <Popover.Content className='popover'>
+          <Card>
+            <Button onClick={logout} >log out</Button>
+            <Popover.Close style={{padding: '3px 3px 0 3px', border: 'none', marginLeft: '3px'}}>{ x }</Popover.Close>
+            <Popover.Arrow className='popover-arrow'/>
+          </Card>
+        </Popover.Content>
+
         <Popover.Trigger asChild >
           <Button >user</Button>
         </Popover.Trigger>
-        <Popover.Content className='popover'>
-          <Box>
-            <Button onClick={logout} >log out</Button>
-            <Popover.Close>x</Popover.Close>
-            <Popover.Arrow className='popover-arrow'/>
-          </Box>
-        </Popover.Content>
+        
       </Popover.Root>
     </Flex>
   )
