25ab8c3bc3f375a34a44458c093caf75841e5297
Lydia Dunning <lydia.dunning@gmail.com>
2024-01-19 12:03:28 -0800

    Update exposure layout

--- frontend/src/components/Exposure.jsx
+++ frontend/src/components/Exposure.jsx
@@ -1,17 +1,32 @@
-import { Button, Card } from '@radix-ui/themes'
+import { IconButton, Card, Flex } from '@radix-ui/themes'
+import { pencil } from '../assets/svgImages'
 
 const Exposure = ({ exposure, openEditExposures }) => {
 
   const date = exposure.date ? new Date(exposure.date) : null
 
   return (
-    <Card>
-      <p>Food: { exposure.food }</p>
-      <p>Description: { exposure.description }</p>
-      { exposure.outcomes.length > 0 && <p>Outcome: { exposure.outcomes.join(', ') }</p> } 
-      { exposure.meal && <p>Meal: { exposure.meal }</p> }
-      { exposure.date && <p>Date: { date.toDateString() }</p>}
-      <Button variant='outline' onClick={ () => openEditExposures(exposure) }>Edit Exposure</Button>
+    <Card m='4'>
+      <Flex gap='2'>
+        <Flex direction='column' >
+
+          <p>{ exposure.food }</p>
+          { exposure.meal && <p>{ exposure.meal }</p> }
+          { exposure.date && <p>{ date.toDateString() }</p>}
+
+        </Flex>
+        <Flex direction='column'>
+
+          <p>{ exposure.description }</p>
+
+        </Flex> 
+      </Flex>
+      <Flex justify='between'>
+
+      { exposure.outcomes.length > 0 && <p>{ exposure.outcomes.join(', ') }</p> } 
+        <IconButton onClick={ () => openEditExposures(exposure) }>{ pencil }</IconButton>
+        
+      </Flex>
     </Card>
   )
 }
