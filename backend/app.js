0f6032394d4eb5f835f3bc9882ba57668cd05fe2
Lydia Dunning <lydia.dunning@gmail.com>
2024-01-29 20:22:57 -0800

    Fix frontend connecting to localhost

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ mongoose.connect(config.MONGODB_URI)
   })
 
 app.use(cors())// enables cors (cross origin resource sharing)
-app.get('http://localhost:5173', cors(), function (req, res, next) {
+app.get(config.PORT, cors(), function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for a Single Route'})
 })
 
