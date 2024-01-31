cdffcb57de3846599ca13f949c45b4fea909b000
Lydia Dunning <lydia.dunning@gmail.com>
2024-01-30 21:45:59 -0800

    move BrowserRouter to main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { HistoryContextProvider } from './components/history/useHistory.jsx'
 import { checkForLogin } from './components/userAuth/userHooks.jsx'
 // import { UserContextProvider } from './components/userAuth/userContext.js'
+import { BrowserRouter } from 'react-router-dom';
 
 const queryClient = new QueryClient()
 const loggedInUser = checkForLogin()
@@ -17,9 +18,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Theme accentColor="amber" grayColor="sand" radius="large" hasBackground='true' scaling="95%" panelBackground='solid'>
       <QueryClientProvider client={queryClient}>
         <HistoryContextProvider loggedInUser={loggedInUser}>  
-        {/* <UserContextProvider user={loggedInUser}> */}
+          <BrowserRouter>
             <App />
-        {/* </UserContextProvider> */}
+          </BrowserRouter>
         </HistoryContextProvider>
       </QueryClientProvider>
     </Theme>
