ae28044c09d87bf2c376636077bef4a3d700c3f1
Lydia Dunning <lydia.dunning@gmail.com>
2024-02-05 12:04:58 -0800

    stop list of kids from loading autoscrolled to add a child element by adding a loading page.

--- frontend/src/components/KidList.jsx
+++ frontend/src/components/KidList.jsx
@@ -2,6 +2,7 @@ import Kid from './Kid.jsx'
 import { IconButton, Flex, Heading, Card } from '@radix-ui/themes'
 import { plus } from '../assets/svgImages.jsx'
 import { useNavigate } from 'react-router-dom'
+import Loader from './Loader.jsx'
 
 export const KidList = ({ kidData, setKid }) => {
   const navigate = useNavigate()
@@ -12,7 +13,7 @@ export const KidList = ({ kidData, setKid }) => {
       <Heading align='center' mt='3' mb='6'>
         Kids
       </Heading>
-      {
+      { kidData ?
         <Flex className='swipe-container' direction='row' gap='3' align='start'>
           {kidData?.map((kid) => (
             <Kid key={kid.id} kid={kid} setKid={setKid} />
@@ -23,6 +24,7 @@ export const KidList = ({ kidData, setKid }) => {
             </IconButton>
           </Card>
         </Flex>
+        : <Loader/>
       }
     </>
   )
---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,11 @@
+const Loader = () => {
+  return (
+    <>
+      <div className='loading'>
+        {/* need to find a loading animation that makes sense when it only appears briefly */}
+      </div>
+    </>
+  )
+}
+
+export default Loader
