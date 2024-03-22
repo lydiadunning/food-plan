e4edc665b7bb207cea4c20220ce93e349943636f
Lydia Dunning <lydia.dunning@gmail.com>
2024-03-22 13:58:52 -0700

    Make up/down/remove outcome buttons type button

--- frontend/src/components/outcomeOptionMenu/Outcome.jsx
+++ frontend/src/components/outcomeOptionMenu/Outcome.jsx
@@ -17,18 +17,18 @@ const Outcome = ({
         <Flex gap='3'>
           {!isFirst && 
           <Tooltip content='move down' delayDuration='1000' skipdelayDuration='100'>
-            <IconButton onClick={moveThisUp}>{upArrow}</IconButton>
+            <IconButton type='button' onClick={moveThisUp}>{upArrow}</IconButton>
           </Tooltip >
           }
           {!isLast ? (
             <Tooltip content='move down' delayDuration='1000' skipdelayDuration='100'>
-              <IconButton onClick={moveThisDown}>{downArrow}</IconButton>
+              <IconButton type='button' onClick={moveThisDown}>{downArrow}</IconButton>
             </Tooltip>
           ) : (
             <Box width='6'></Box>
           )}
           <Tooltip content='remove' delayDuration='1000' skipdelayDuration='100'>
-            <IconButton onClick={removeThis}>{x}</IconButton>
+            <IconButton type='button' onClick={removeThis}>{x}</IconButton>
           </Tooltip>
         </Flex>
       </Flex>
